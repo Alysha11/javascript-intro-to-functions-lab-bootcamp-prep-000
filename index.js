1a0a3ea2244de0e725d45fc27cc16565097258a9
cygnet_alesha@hotmail.com <cygnet_alesha@hotmail.com>
2018-04-12 15:51:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function shout (string) {
 function whisper (string) {
   return string.toLowerCase()
 }
-function sayHiToGrandma (string) {
-  if (string===string.toLowerCase())
-  return "I can\'t hear you!";
+
+function sayHitoGrandma(string) {
+  if (string === string.toLowerCase)
+return "I can\'t hear you!";
+  
 }
