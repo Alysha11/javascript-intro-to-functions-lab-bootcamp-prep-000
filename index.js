8ce97240b62c91be0b9dfde6f8ae6a443549e15b
cygnet_alesha@hotmail.com <cygnet_alesha@hotmail.com>
2018-04-12 16:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,15 +19,28 @@ function logWhisper (string) {
   console.log (string.toLowerCase())
 }
 
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
 
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase())
+  if (string === lowercase())
 return 'I can\'t hear you!';
   
 }
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase())
+  if (string === uppercase())
 return "YES INDEED!";
   
 }
