4ee71b92ab6eec1ae8fbdbcae30affc92e3098a9
cygnet_alesha@hotmail.com <cygnet_alesha@hotmail.com>
2018-04-12 15:59:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function whisper (string) {
   return string.toLowerCase()
 }
 
+function logShout (string) {
+  
+  console.log (string.toUpperCase())
+}
+
+
+
+
+
+
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
 return "I can\'t hear you!";
